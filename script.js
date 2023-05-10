46ba974e4442758b7e96d42529684c1b81076e9f
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-10 23:55:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+    let string = str.replaceAll(" " , "");
+	let s = string;
+	let s1 = "";
+	for(let x=s.length-1;x>=0;x--){
+		s1+=s[x];
+	}
+	if(s1 === string){
+		console.log(true);
+	}
+	else{
+		console.log(false);
+	}
 }
+
 module.exports = palindrome
